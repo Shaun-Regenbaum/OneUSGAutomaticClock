f2b06db2e826a359833859afa22c7da98767f431
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-01-10 14:28:37 -0500

    Fixed and updated code

--- clock_manager.py
+++ clock_manager.py
@@ -1,216 +1,242 @@
-#=================================================================================================#
-# pip install selenium
-# Add the chromedriver.exe to your environment path
 
-# You are good to go
 import time
-from selenium import webdriver 
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium import webdriver
+import chromedriver_autoinstaller
+
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 
-#User Variables:
-# Put how many hours you want here, the time will be rounded to the closest 15 minute mark:
-hours_to_clock = 1
-# Put in your username and password to login:
-username = "username"
-password = "password" 
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+
+#=================================================================================================#
+
+# User Variables (THINGS YOU NEED TO CHANGE):
+#   Put how many hours you want here, the time will be rounded to the closest 15 minute mark:
+HOURS_TO_CLOCK = .5
+#   Put in your USERNAME and PASSWORD to login:
+USERNAME = "username"
+PASSWORD = "password"
 
 
-# Make sure to update this path:
-path_to_driver = "/Users/shaunregenbaum/Dropbox (GaTech)/Programming/Python/Work Scripts/Georgia Tech Payments/chromedriver"
+#=================================================================================================#
+#=================================================================================================#
 
 # You shouldn't need to change anything past this point (besides duo 2fa), but you do you
+
+#=================================================================================================#
 #=================================================================================================#
 
+
 # Global Variables:
-minutes = hours_to_clock * 60
-time_blocks = round(minutes / 15)
-blocks_done = 0
-driver = webdriver.Chrome(path_to_driver)
-wait = WebDriverWait(driver, 25)
+chromedriver_autoinstaller.install()
 
+MINUTES = HOURS_TO_CLOCK * 60
+TIME_BLOCKS = round(MINUTES / 15)
+BLOCKS_DONE = 0
+DRIVER = webdriver.Chrome()
+WAIT = WebDriverWait(DRIVER, 25)
+DEBUG_MODE = False
 
-
+#=================================================================================================#
 # Functions, each step gets its own function:
 
-def goToLogin():
-    driver.get("https://selfservice.hprod.onehcm.usg.edu/psc/hprodsssso_newwin/HCMSS/HRMS/c/HGA_EMPLOYEE_FL.BOR_TIME_NAV_FLU.GBL")
-    
-    try:
-        gt_option = driver.find_element_by_xpath("//*[@id='https_idp_gatech_edu_idp_shibboleth']/div/div/a")
-        gt_option.send_keys(Keys.RETURN)
-        return 1
-    except:
-        print("Error, cannot find login")
-        return 0
 
-def login(status):
-    if not status:
-        return 0
+# Go To One USG:
+def goToOneUSG():
+    DRIVER.get("https://hcm-sso.onehcm.usg.edu/")
 
-    try:
-        wait.until(lambda driver: driver.find_element_by_name("username"))
-        gatech_login_username = driver.find_element_by_name("username")
-        gatech_login_password = driver.find_element_by_name("password")
-
-        gatech_login_username.send_keys(username)
-        gatech_login_password.send_keys(password)
-
-        gatech_login_password.send_keys(Keys.RETURN)
-
-    except:
-        print("Error Cannot Find the Username/Password Form")
-        return 0
-    
-    try:
-        print("Script will wait 25 seconds for you to authenticate on duo")
-        print("If you run out of time, just run the script again")
-        
-        wait.until(lambda driver: driver.find_elements_by_id("duo_form"))
-        return 1
-    except:
-        print("Error, Incorrect Username/Password")
-        print("Error, Duo Malfunction?")
-        return 0
-
-def goToClock(status):
-    if not status:
-        return 0
-
-    prevent_timeout()
-
-    try:
-        wait.until(lambda driver: driver.find_element_by_id("BOR_INSTALL_VW$0_row_0"))
-        clock_button = driver.find_element_by_id("BOR_INSTALL_VW$0_row_0")
-        clock_button.send_keys(Keys.RETURN)
-
-        return 1
-    except:
-        # Sometimes there is a wierd error where the clock dissappear so we just reload the page and try again:
-        driver.refresh()
-        try:
-            wait.until(lambda driver: driver.find_element_by_id("BOR_INSTALL_VW$0_row_0"))
-            clock_button = driver.find_element_by_id("BOR_INSTALL_VW$0_row_0")
-            clock_button.send_keys(Keys.RETURN)
-            return 1
-        except:
-            return 0
-
-def clockHoursIn(status):
-    if not status:
-        return 0
+    return error_handler(element_to_find="//*[@id='https_idp_gatech_edu_idp_shibboleth']/div/div/a", method_to_find="xpath", purpose="Going to One Usg")
 
 
-    wait.until(lambda driver: driver.find_element_by_id("ptifrmtgtframe"))
-    driver.switch_to.frame("ptifrmtgtframe")
-          
-    drop_down_menu = Select(driver.find_element_by_id("TL_RPTD_TIME_PUNCH_TYPE$0"))
+# Selecting GT:
+def selectGT():
+    gt_option = DRIVER.find_element_by_xpath(
+        "//*[@id='https_idp_gatech_edu_idp_shibboleth']/div/div/a")
+    gt_option.send_keys(Keys.RETURN)
+
+    return error_handler(element_to_find="username", method_to_find="name", purpose="Selecting GT")
+
+
+# This is an alternative function to try go straight to the GT Login Page, but its not used for now.
+def goToGT():
+    DRIVER.get("https://idpproxy.usg.edu/asimba/sso/web?asid=H--OuU7aI8l1f5IFN1anGQ&saml_organization_id=https://idp.gatech.edu/idp/shibboleth")
+    return 1
+
+# This function logs us in once we are at the GT login Page:
+
+
+def loginGT():
+    gatech_login_username = DRIVER.find_element_by_name("username")
+    gatech_login_password = DRIVER.find_element_by_name("password")
+
+    gatech_login_username.send_keys(USERNAME)
+    gatech_login_password.send_keys(PASSWORD)
+
+    submit_button = DRIVER.find_element_by_name("submit")
+    submit_button.click()
+
+    print("Script will wait 25 seconds for you to authenticate on duo")
+    print("If you run out of time, just run the script again")
+    return error_handler(element_to_find="duo_form", method_to_find="id", purpose="Logging In")
+
+
+# This function goes through the menus to get us to the clock:
+def goToClock():
+
+    WAIT.until(lambda DRIVER: DRIVER.find_element_by_id(
+        "win0divPTNUI_LAND_REC_GROUPLET$4"))
+
+    time_and_absence_button = DRIVER.find_element_by_id(
+        "win0divPTNUI_LAND_REC_GROUPLET$4")
+    time_and_absence_button.send_keys(Keys.RETURN)
+
+    clock_button = DRIVER.find_element_by_id("BOR_INSTALL_VW$0_row_0")
+    clock_button.send_keys(Keys.RETURN)
+
+    return error_handler(element_to_find="ptifrmtgtframe", method_to_find="id", purpose="Going to the Clock")
+
+# This function clocks us in:
+
+
+def clockHoursIn():
+
+    DRIVER.switch_to.frame("ptifrmtgtframe")
+
+    drop_down_menu = Select(
+        DRIVER.find_element_by_id("TL_RPTD_TIME_PUNCH_TYPE$0"))
     drop_down_menu.select_by_value("1")
 
-    punch_button = driver.find_element_by_id("TL_LINK_WRK_TL_SAVE_PB$0")
+    punch_button = DRIVER.find_element_by_id("TL_LINK_WRK_TL_SAVE_PB$0")
     punch_button.send_keys(Keys.RETURN)
 
-    driver.switch_to.default_content()
+    DRIVER.switch_to.default_content()
 
-    wait.until(lambda driver: driver.find_element_by_id("#ICOK"))
-    popup_button = driver.find_element_by_id("#ICOK")
+    WAIT.until(lambda DRIVER: DRIVER.find_element_by_id("#ICOK"))
+    popup_button = DRIVER.find_element_by_id("#ICOK")
     popup_button.send_keys(Keys.RETURN)
-    try:
-  
 
-        print("You Have Clocked In, Be Careful That Your Computer Does Not Turn Off")
-       
-        return 1
-    except:
-        print("Error, Unable to Clock In")
-        return 0
+    print("You Have Clocked In, Be Careful That Your Computer Does Not Turn Off.")
 
-def clockHoursOut(status):
-    if not status:
-        return 0
+    return error_handler(element_to_find="ptifrmtgtframe", method_to_find="id", purpose="Clocking In")
 
-    prevent_timeout()
+
+# This function clocks us out:
+def clockHoursOut():
 
     try:
-        wait.until(lambda driver: driver.find_element_by_id("ptifrmtgtframe"))
-        driver.switch_to.frame("ptifrmtgtframe")
+        DRIVER.switch_to.frame("ptifrmtgtframe")
 
-        drop_down_menu = Select(driver.find_element_by_id("TL_RPTD_TIME_PUNCH_TYPE$0"))
+        drop_down_menu = Select(
+            DRIVER.find_element_by_id("TL_RPTD_TIME_PUNCH_TYPE$0"))
         drop_down_menu.select_by_value("2")
 
-        punch_button = driver.find_element_by_id("TL_LINK_WRK_TL_SAVE_PB$0")
+        punch_button = DRIVER.find_element_by_id("TL_LINK_WRK_TL_SAVE_PB$0")
         punch_button.send_keys(Keys.RETURN)
 
-        driver.switch_to.default_content()
+        DRIVER.switch_to.default_content()
 
-        wait.until(lambda driver: driver.find_element_by_id("#ICOK"))
-        popup_button = driver.find_element_by_id("#ICOK")
+        WAIT.until(lambda driver: driver.find_element_by_id("#ICOK"))
+        popup_button = DRIVER.find_element_by_id("#ICOK")
         popup_button.send_keys(Keys.RETURN)
 
         print("You Have Clocked Out")
-        driver.quit()
+        DRIVER.quit()
         return 1
 
-    except:
-        print("Error, Unable to Clock Out")
+    except (RuntimeError, TypeError, NameError, NoSuchElementException, TimeoutException) as error:
+        print("Failed, Unable to Clock Out.")
+        print("NOTICE: Please Manually Clock Out To Avoid Issues")
+        print("If this error continues, please raise an issue on Github")
+
+        if not DEBUG_MODE:
+            DRIVER.quit()
+        else:
+            print("Debug Mode:")
+            print(error)
         return 0
 
-def goBackToMenu(status):
-    if not status:
-        return 0
 
-    global blocks_done 
-    blocks_done = blocks_done + 1
-
-    prevent_timeout()
-
-    try:
-        driver.switch_to.default_content()
-        wait.until(lambda driver: driver.find_element_by_id("PT_WORK_PT_BUTTON_BACK"))
-        back_button = driver.find_element_by_id("PT_WORK_PT_BUTTON_BACK")
-        back_button.send_keys(Keys.RETURN)
-
-        print("Up To: " + str(blocks_done*15) + " Minutes")
-        return 1
-
-    except:
-        print("Error, Cannot Find Back Button")
-        return 0
-
+# This function prevents timeouts:
 def prevent_timeout():
-    driver.refresh()
-    
+    DRIVER.refresh()
+
+    # If the timeout box does appear, click the prevent timeout button
     try:
-        timeout_button = driver.find_element_by_id("BOR_INSTALL_VW$0_row_0")
+        timeout_button = DRIVER.find_element_by_id("BOR_INSTALL_VW$0_row_0")
         timeout_button.send_keys(Keys.RETURN)
         print("Timeout Prevented")
-        return()
-    except:
-        return()
+        return 1
+    except (NoSuchElementException, TimeoutException):
+        return 1
+
+
+# This function handles errors and returns either one or zero to indicate success or failure:
+def error_handler(element_to_find, method_to_find="id", purpose="Default, Please Specify when Invoking error_handler"):
+    try:
+        if method_to_find == "xpath":
+            WAIT.until(
+                lambda DRIVER: DRIVER.find_element_by_xpath(element_to_find))
+            return 1
+        elif method_to_find == "id":
+            WAIT.until(
+                lambda DRIVER: DRIVER.find_element_by_id(element_to_find))
+            return 1
+        elif method_to_find == "name":
+            WAIT.until(
+                lambda DRIVER: DRIVER.find_element_by_name(element_to_find))
+            return 1
+        else:
+            print("method_to_find not right")
+            if not DEBUG_MODE:
+                DRIVER.quit()
+            else:
+                print("Debug Mode:")
+            return 0
+
+    except (NoSuchElementException, TimeoutException) as error:
+        print("The Element was not found, this means OneUsg made some changes.")
+        print("If this error continues, please raise an issue on Github.")
+
+        if not DEBUG_MODE:
+            DRIVER.quit()
+        else:
+            print("Debug Mode:")
+            print(error)
+        return 0
+
+    except (RuntimeError, TypeError, NameError) as error:
+        print("Failure with: " + purpose)
+        if not DEBUG_MODE:
+            DRIVER.quit()
+        else:
+            print("Debug Mode:")
+            print(error)
+        return 0
+
 
 #=================================================================================================#
 # The script running:
 
-a = goToLogin()
-b = login(a)
-c = goToClock(b)
+goToOneUSG()
+selectGT()
+loginGT()
+goToClock()
+clockHoursIn()
 
-d = clockHoursIn(c)
-
-# This is to prevent timing out:
-while blocks_done < time_blocks:
-    e = goBackToMenu(d)
-    time.sleep(225)
+while BLOCKS_DONE < TIME_BLOCKS:
+    print(str(BLOCKS_DONE*15) + " minutes done, " +
+          str(MINUTES - BLOCKS_DONE*15) + " minutes left to go.")
     prevent_timeout()
-    time.sleep(225)
-    f = goToClock(e)
-    # Run through every 15 minutes, the timeout happens at 20 minutes
-    time.sleep(225)
-    prevent_timeout()
-    time.sleep(225)
+    time.sleep(300)
+    if BLOCKS_DONE == TIME_BLOCKS:
+        break
+    BLOCKS_DONE = BLOCKS_DONE + 1
 
 else:
-    g = clockHoursOut(d)
+    clockHoursOut()
+
 
 #=================================================================================================#
